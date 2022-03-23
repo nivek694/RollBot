bb57dbf9d6039f522fdbc54003fc930f2a97d832
Kevin Hayes <nivek694@gmail.com>
2022-03-23 17:51:26 -0400

    Mase startup work

--- First_Time_Startup.py
+++ First_Time_Startup.py
@@ -1,7 +1,7 @@
 
 def _rollbot_config():
     name = input("Enter bot token")
-    f = open("config.ini", "x")
+    f = open("config.ini", "w")
 
     f.write("[BOT]\nRollBot = %s\n[ADMIN]\nadminID = 393122454236692480" % name)
     f.close()
